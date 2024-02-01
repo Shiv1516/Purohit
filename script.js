7f5ac929d7806f7b61cb7faae5820841a32bd1fc
urfav-ravi <mr.ravi13713@gmail.com>
2024-02-01 17:43:49 +0530

    update files

--- script.js
+++ script.js
@@ -9,3 +9,51 @@ menuButton.addEventListener("click", () => {
     navBar.classList.remove("on-nav");
   }
 });
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper2 = new Swiper(".swiper2", {
+  slidesPerView:
+      window.innerWidth <= 450
+          ? 1
+          : window.innerWidth <= 1025
+              ? 2
+              : window.innerWidth <= 1400
+                  ? 3
+                  : 4,
+  direction: getDirection(),
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? "horizontal" : "horizontal";
+
+  return direction;
+}
+
+
+
+// temple-service hero slider
+
+var swiper = new Swiper(".hero-mySwiper", {
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? "horizontal" : "horizontal";
+
+  return direction;
+}
